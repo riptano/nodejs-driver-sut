8a5a7c67a8e0c553e32343f9b8c1b09a7592ae8c
Jorge Bay Gondra <jorge.gondra@datastax.com>
2015-08-28 11:26:22 +0200

    Use config for build and run scripts

--- build.py
+++ build.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import argparse
+import shutil
 
 
 def exec_command(command):
@@ -11,11 +12,29 @@ def exec_command(command):
     if ret != 0:
         sys.exit(1)
 
-parser = argparse.ArgumentParser(description="Node.js Driver SUT", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--branch', required=False, default="master", help="Node.js driver branch to be used")
+def configuration_file(value):
+    if os.path.isfile(value):
+        return value
+    else:
+        raise argparse.ArgumentTypeError("%s does not exist" % value)
+
+
+parser = argparse.ArgumentParser(description="Node.js Driver SUT build",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--configuration-file", required=False, type=configuration_file,
+                    help="The configuration file that should be used")
 args = parser.parse_args()
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
+print "# Configuration file: %s" % (args.configuration_file)
 print "# Current directory: %s" % (current_directory)
-exec_command("npm install datastax/nodejs-driver#%s" % (args.branch))
-exec_command("npm install")
+
+branch = "master"
+if args.configuration_file is not None:
+    import ConfigParser
+    config = ConfigParser.RawConfigParser({"driver_branch": branch})
+    config.read(args.configuration_file)
+    branch = config.get("build", "driver_branch")
+
+exec_command("npm install datastax/nodejs-driver#%s --prefix %s" % (branch, current_directory))
+exec_command("npm install --prefix %s" % (current_directory))
--- run.py
+++ run.py
@@ -2,7 +2,38 @@
 
 import sys
 import os
+import argparse
+import shutil
 
-command = "node src/server.js"
+def configuration_file(value):
+    if os.path.isfile(value):
+        return value
+    else:
+        raise argparse.ArgumentTypeError("%s does not exist" % value)
+
+parser = argparse.ArgumentParser(description="Node.js Driver SUT build",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--configuration-file", required=False, type=configuration_file,
+                    help="The configuration file that should be used")
+args = parser.parse_args()
+
+current_directory = os.path.dirname(os.path.realpath(__file__))
+print "# Configuration file: %s" % (args.configuration_file)
+print "# Current directory: %s" % (current_directory)
+
+contact_points = "127.0.0.1"
+graphite_host = "127.0.0.1"
+queries_per_http = 1
+if args.configuration_file is not None:
+    import ConfigParser
+    config = ConfigParser.RawConfigParser({"cassandra_contact_points": contact_points,
+                                           "metrics_export_graphite_host": graphite_host,
+                                           "cql_queries_per_http_request": queries_per_http})
+    config.read(args.configuration_file)
+    contact_points = config.get("run", "cassandra_contact_points")
+    graphite_host = config.get("run", "metrics_export_graphite_host")
+    queries_per_http = config.get("run", "cql_queries_per_http_request")
+
+command = "node %s/src/server.js %s %s" % (current_directory, contact_points, graphite_host)
 print "# Running command: %s" % (command)
 sys.exit(os.system(command))
