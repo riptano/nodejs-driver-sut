f0e4e8084e4058be0569a739b84fdd322f690f91
Jorge Bay Gondra <jorge.gondra@datastax.com>
2015-09-23 14:18:00 +0200

    Standalone throughput test: include select query

--- test/throughput-test.js
+++ test/throughput-test.js
@@ -3,30 +3,42 @@ var assert = require('assert');
 var async = require('async');
 var types = cassandra.types;
 var fs = require('fs');
+var util = require('util');
 
 var contactPoint = process.argv[2] || '127.0.0.1';
 var client = new cassandra.Client({
   contactPoints: [ contactPoint ],
   keyspace: process.argv[3] || 'killrvideo',
+  policies: { loadBalancing: new cassandra.policies.loadBalancing.DCAwareRoundRobinPolicy('datacenter1')},
   encoding: { copyBuffer: false},
-  socketOptions: { tcpNoDelay: false },
-  pooling: { coreConnectionsPerHost: {'0': parseInt(process.argv[3]) || 8, '1': 1, '2': 0} }
+  socketOptions: { tcpNoDelay: true },
+  pooling: { coreConnectionsPerHost: {'0': parseInt(process.argv[4], 10) || 1, '1': 1, '2': 0} }
 });
 var insertQuery = 'INSERT INTO comments_by_video (videoid, userid, commentid, comment) VALUES (?, ?, ?, ?)';
-var selectQuery = 'SELECT videoid, userid, commentid, comment FROM comments_by_video WHERE videoid = ?';
-var ops = 10000;
+var selectQuery = 'SELECT videoid, userid, commentid, comment FROM comments_by_video WHERE videoid = ? and commentid = ?';
+var ops = 20000;
 var ids = Array.apply(null, new Array(10)).map(function () { return types.Uuid.random();});
 var userId = types.Uuid.random();
+var selectParams;
 
 async.series([
   client.connect.bind(client),
   function warmup(seriesNext) {
-    async.timesSeries(10, function (n, next) {
+    async.times(100, function (n, next) {
       var params = [types.Uuid.random(), userId, types.TimeUuid.now(), 'hello!'];
-      client.execute(insertQuery, params, { prepare: 1}, next);
+      //it will be later be used for selecting
+      selectParams = [params[0], params[2]];
+      async.series([
+        function (warmupNext) {
+          client.execute(insertQuery, params, { prepare: 1, consistency: types.consistencies.all}, warmupNext);
+        },
+        function (warmupNext) {
+          client.execute(selectQuery, selectParams, { prepare: 1, consistency: types.consistencies.all}, warmupNext);
+        }
+      ], next);
     }, function (err) {
-      console.log('Starting tests');
       assert.ifError(err);
+      console.log('Starting tests');
       console.log('%d Connections per host', client.hosts.slice(0)[0].pool.connections.length);
       var driverVersion = JSON.parse(fs.readFileSync('node_modules/cassandra-driver/package.json', 'utf8')).version;
       console.log('Driver v%s', driverVersion);
@@ -52,7 +64,7 @@ async.series([
         assert.ifError(err);
         var end = process.hrtime(start);
         if (n === 0) {
-          console.log('Inserted %d rows', arr.length);
+          console.log('Executed %d times', arr.length);
         }
         else {
           process.stdout.write('.');
@@ -70,27 +82,38 @@ async.series([
       seriesNext(err);
     });
   },
-  function select (seriesNext) {
+  function select(seriesNext) {
     console.log('Starting select test');
     var elapsed = [];
+    var arr = Array.apply(null, new Array(ops)).map(function () { return types.TimeUuid.now() });
     async.timesSeries(10, function (n, timesNext) {
-      async.eachLimit(ids, 3, function (id, next) {
-        var start = process.hrtime();
-        var retrieved = 0;
-        client.eachRow(selectQuery, [id], { prepare: true, autoPage: true}, function (n, row) {
-          //use the row value for something
-          retrieved += row['videoid'].toString().length;
-        }, function (err) {
-          assert.ifError(err);
-          var end = process.hrtime(start);
-          elapsed.push(end[0] + end[1] / 1000000000);
-          next();
+      var start = process.hrtime();
+      var id = ids[n];
+      if (!id) {
+        return timesNext();
+      }
+      async.eachLimit(arr, 50, function (tid, eachNext) {
+        client.execute(selectQuery, selectParams, { prepare: true, consistency: types.consistencies.one}, function (err) {
+          eachNext(err);
         });
-      }, timesNext);
+      }, function (err) {
+        assert.ifError(err);
+        var end = process.hrtime(start);
+        if (n === 0) {
+          console.log('Executed %d times', arr.length);
+        }
+        else {
+          process.stdout.write('.');
+        }
+        elapsed.push(end[0] + end[1] / 1000000000);
+        timesNext();
+      });
     }, function (err) {
+      process.stdout.write('\n');
       var meanElapsed = mean(elapsed);
       console.log('mean:   %s secs', meanElapsed.toFixed(6));
       console.log('median: %s secs', median(elapsed).toFixed(6));
+      console.log('throughput (mean): %s ops/sec', ~~(ops / meanElapsed));
       console.log('-------------------------');
       seriesNext(err);
     });
