e59a466ec3edfa8b0861ed8afeba85d83a45f5af
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2018-10-04 12:58:28 +0200

    Latency benchmark: use workloads

--- test/basic-latency-test.js
+++ test/basic-latency-test.js
@@ -2,33 +2,23 @@
 
 const fs = require('fs');
 const hdr = require('hdr-histogram-js');
-const dse = require('dse-driver');
+const clientFactory = require('../src/client-factory');
 
-const iterations = 200;
-const timesPerIteration = 100;
+const iterations = 5000;
+const timesPerIteration = 200;
 const delay = 20;
 
 const histogram = hdr.build({
   bitBucketSize: 32,
-  highestTrackableValue: 20000,
-  numberOfSignificantValueDigits: 5
+  highestTrackableValue: 400000,
+  numberOfSignificantValueDigits: 4
 });
 
-async function connect() {
-  const contactPoints = [ process.argv[2] || '127.0.0.1' ];
-  const client = new dse.Client({ contactPoints, protocolOptions: { maxVersion: 4 } });
-
-  await client.connect();
-  return client;
-}
-
-async function startBenchmark(client) {
+async function startBenchmark(client, workload) {
   console.log(`Getting started using hosts: ${client.hosts.values().map(h => h.address)}`);
 
-  // warmup
-  await Promise.all(new Array(128).fill(0).map(() => {
-    client.execute('SELECT * FROM system.local');
-  }));
+  await workload.init();
+  await workload.warmup();
 
   let firstError;
   let iterationCounter = 0;
@@ -41,21 +31,12 @@ async function startBenchmark(client) {
     }
 
     for (let i = 0; i < timesPerIteration; i++) {
-      const startTime = process.hrtime();
-      client.execute('SELECT * FROM system.local', (err) => {
-        if (firstError) {
-          return;
-        }
-
-        if (err) {
+      workload.execute(err => {
+        if (err && !firstError) {
           firstError = err;
           clearInterval(intervalTimer);
           console.error(err);
         }
-
-        const diff = process.hrtime(startTime);
-        const latencyMs = diff[0] * 1000000 + diff[1] / 1000;
-        histogram.recordValue(latencyMs);
       });
     }
   }, delay);
@@ -70,5 +51,93 @@ function finish(client) {
   setTimeout(() => client.shutdown(), 1000);
 }
 
-connect()
-  .then(client => startBenchmark(client));
+clientFactory
+  .createAndConnect()
+  .then(client => startBenchmark(client, new MixedWorkload(client, histogram, 1)));
+
+class BasicWorkload {
+  constructor(client, histogram) {
+    this.client = client;
+    this.histogram = histogram;
+  }
+
+  async createSchema() {
+
+  }
+
+  async warmup() {
+    await Promise.all(new Array(256).fill(0).map(() => {
+      this.client.execute('SELECT * FROM system.local');
+    }))
+  }
+
+  execute(callback) {
+    const startTime = process.hrtime();
+    this.client.execute('SELECT * FROM system.local', (err) => {
+      const diff = process.hrtime(startTime);
+      const latencyMs = diff[0] * 1000000 + diff[1] / 1000;
+      this.histogram.recordValue(latencyMs);
+      callback(err);
+    });
+  }
+}
+
+class MixedWorkload {
+  constructor(client, histogram, replicationFactor) {
+    this.client = client;
+    this.histogram = histogram;
+    this.replicationFactor = replicationFactor;
+    this.insertQuery = 'INSERT INTO standard1 (key, c0, c1) VALUES (?, ?, ?)';
+    this.selectQuery = 'SELECT c0 FROM standard1 WHERE key = ?';
+    this.queryOptions = { prepare: true };
+    this.executeIndex = 0;
+  }
+
+  async init() {
+    const queries = [
+      `USE ks_benchmarks_rf${this.replicationFactor}`,
+      'DROP TABLE IF EXISTS standard1',
+      'CREATE TABLE standard1 (key blob PRIMARY KEY,c0 blob,c1 blob,c2 blob,c3 blob,c4 blob)'
+    ];
+
+    for (const q of queries) {
+      await this.client.execute(q);
+    }
+  }
+
+  async warmup() {
+    const b = Buffer.alloc(4);
+
+    await Promise.all(new Array(256).fill(0).map(() =>
+      this.client.execute(this.insertQuery, [b, b, b], this.queryOptions)));
+
+    await Promise.all(new Array(256).fill(0).map(() =>
+      this.client.execute(this.selectQuery, [b], this.queryOptions)));
+  }
+
+  execute(callback) {
+    const index = this.executeIndex++;
+    const b = Buffer.allocUnsafe(4);
+    const startTime = process.hrtime();
+
+    let query;
+    let params;
+
+    if (index % 2 === 0) {
+      query = this.selectQuery;
+      b.writeInt32BE(index / 2, 0);
+      params = [ b ]
+    } else {
+      query = this.insertQuery;
+      b.writeInt32BE(index, 0);
+      params = [ b, b, b ]
+    }
+
+    this.client.execute(query, params, this.queryOptions, (err) => {
+      const diff = process.hrtime(startTime);
+      const latencyMs = diff[0] * 1e9 + diff[1] / 1e3;
+      this.histogram.recordValue(latencyMs);
+      callback(err);
+    });
+  }
+}
