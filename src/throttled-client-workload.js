bdc80e67034e6974e0eda907dad5dd9817e62774
Andrew Tolbert <andrew.tolbert@datastax.com>
2018-12-10 12:50:33 -0600

    Revert "Add capability to run with stepped outstanding requests"
    This reverts commit 24cac55955c2a1d18da615d5ddb825faec5e18dc.

--- src/throttled-client-workload.js
+++ src/throttled-client-workload.js
@@ -4,7 +4,7 @@ var utils = require('./utils');
 var currentMicros = utils.currentMicros;
 var metrics = require('metrics');
 
-var warmupLength = 50000;
+var warmupLength = 256;
 
 /**
  * @classdesc
@@ -117,89 +117,58 @@ ClientWorkload.prototype._warmup = function (callback) {
 ClientWorkload.prototype._runWorkloadItems = function (callback) {
   this._logMessage('Running %d workload items');
   var options = this._commandLineOptions;
-  let outstandingTargets = [];
-  if (options.outstandingBase > 0) {
-    for (let i = options.outstandingBase; i < options.outstanding; i+= options.outstandingStep) {
-      outstandingTargets.push(i);
+  utils.eachSeries(this._items, (item, next) => {
+    let totalTimer;
+    console.log('---  %s / %s  ---', this._name, item.name);
+    if (this._commandLineOptions.measureLatency) {
+      totalTimer = new metrics.Timer();
     }
-  }
-  if (outstandingTargets.length == 0 || outstandingTargets[outstandingTargets.length - 1] != options.outstanding) {
-    outstandingTargets.push(options.outstanding);
-  }
-
-  let outstandingPerName = {};
-  this._items.forEach((item) => outstandingPerName[item.name] = []);
-
-  utils.eachSeries(outstandingTargets, (outstanding, stepNext) => {
-    utils.eachSeries(this._items, (item, next) => {
-      let totalTimer;
-      console.log('---  %s / %s @ %d outstanding ---', this._name, item.name, outstanding);
-      if (this._commandLineOptions.measureLatency) {
-        totalTimer = new metrics.Timer();
-      }
-      utils.logTimerHeader();
-      let handler;
-      let seriesTimer;
-      let intervalCount = 0;
-      if (this._commandLineOptions.measureLatency) {
-        seriesTimer = new metrics.Timer();
-        handler = (n, timesNext) => {
-          var queryStart = currentMicros();
-          item.fn(this._client, n, function (err) {
-            var duration = currentMicros() - queryStart;
-            seriesTimer.update(duration);
-            totalTimer.update(duration);
-            timesNext(err);
-          });
-        };
-      }
-      else {
-        handler = (n, timesNext) => {
-          item.fn(this._client, n, function (err) {
-            intervalCount++;
-            timesNext(err);
-          });
-        };
-      }
-
-      let lastTotalCount = 0;
-      let start = process.hrtime();
-      const onInterval = () => {
-        if (totalTimer) {
-          intervalCount = totalTimer.count() - lastTotalCount;
-          lastTotalCount = totalTimer.count();
-        }
-        utils.logTimer(seriesTimer, null, null, intervalCount);
-        intervalCount = 0;
-        if (seriesTimer) {
-          seriesTimer.clear();
-        }
-      };
-
-      utils.timesPerSec(options.ops, outstanding, options.throttle, handler, onInterval, (err) => {
-        const elapsed = process.hrtime(start);
-        const millis = elapsed[0] * 1000 + elapsed[1] / 1000000;
-        utils.logTotals(totalTimer, millis, options.ops);
-        outstandingPerName[item.name].push({
-          target: outstanding,
-          elapsed: millis,
-          timer: totalTimer
+    utils.logTimerHeader();
+    let handler;
+    let seriesTimer;
+    let intervalCount = 0;
+    if (this._commandLineOptions.measureLatency) {
+      seriesTimer = new metrics.Timer();
+      handler = (n, timesNext) => {
+        var queryStart = currentMicros();
+        item.fn(this._client, n, function (err) {
+          var duration = currentMicros() - queryStart;
+          seriesTimer.update(duration);
+          totalTimer.update(duration);
+          timesNext(err);
         });
-        next(err);
-      });
-    }, (err) => {
-      stepNext(err);
+      };
+    }
+    else {
+      handler = (n, timesNext) => {
+        item.fn(this._client, n, function (err) {
+          intervalCount++;
+          timesNext(err);
+        });
+      };
+    }
+
+    let lastTotalCount = 0;
+    let start = process.hrtime();
+    const onInterval = () => {
+      if (totalTimer) {
+        intervalCount = totalTimer.count() - lastTotalCount;
+        lastTotalCount = totalTimer.count();
+      }
+      utils.logTimer(seriesTimer, null, null, intervalCount);
+      intervalCount = 0;
+      if (seriesTimer) {
+        seriesTimer.clear();
+      }
+    };
+
+    utils.timesPerSec(options.ops, options.outstanding, options.throttle, handler, onInterval, (err) => {
+      const elapsed = process.hrtime(start);
+      const millis = elapsed[0] * 1000 + elapsed[1] / 1000000;
+      utils.logTotals(totalTimer, millis, options.ops);
+      next(err);
     });
   }, (err) => {
-    console.log('Final Summary:');
-    Object.keys(outstandingPerName).forEach((name) => {
-      console.log('--- %s ---', name);
-      utils.logTimerHeaderForTarget();
-      let outstanding = outstandingPerName[name];
-      outstanding.forEach((t) => {
-        utils.logTimerForTarget(t.target, t.timer, t.elapsed, null, options.ops);
-      });
-    });
     callback(err);
   });
 };
