ed17e4104649079efc39170cc7a6bfeb82edf5f5
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2019-12-16 15:58:55 +0100

    Add client config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     "es6": true
   },
   "parserOptions": {
-    "ecmaVersion": 2019
+    "ecmaVersion": 2017
   },
   "extends": "eslint:recommended",
   "rules": {
---
+++ config/client-config.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Returns an object containing configuration properties that will be
+// merged to the fixed options (contactPoints, localDataCenter)
+module.exports = function (driver) {
+  return {};
+};
